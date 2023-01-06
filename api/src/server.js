148e5d6c3bd2a9f195bae2b4b583a0bf3f92e0fd
André Knob <knobandre@gmail.com>
2023-01-06 11:49:35 -0300

    feat: addind kafka inititialization

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import { Kafka } from 'kafkajs';
 
 const app = express();
+
+const kafka = new Kafka({
+    clientId: 'api',
+    brokers: ['kafka:9092']
+})
 
 app.post('/certifications', (req, res) => {
     return res.json({ ok: true });
