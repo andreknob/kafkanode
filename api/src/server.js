c64f154121c6d0d38f8986451abd431c68fdb596
André Knob <knobandre@gmail.com>
2023-01-06 15:24:24 -0300

    chore: adding routes

--- api/src/server.js
+++ api/src/server.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import { Kafka } from 'kafkajs';
 
+import routes from './routes';
+
 const app = express();
 
 const kafka = new Kafka({
     clientId: 'api',
     brokers: ['kafka:9092']
-})
-
-app.post('/certifications', (req, res) => {
-    return res.json({ ok: true });
 });
 
-app.listen(3333);
+const producer = kafka.producer();
+
+app.use((req, res, next) => {
+    req.producer = producer;
+
+    return next();
+});
+
+app.use(routes);
+
+
+async function run() {
+    //await producer.connect();
+
+    app.listen(3333);
+}
+
+run().catch(console.error);
